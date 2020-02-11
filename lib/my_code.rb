64128b183abedb4301228898727c9864401deb4d
Gene Campbell III <gcampbell391@gmail.com>
2020-02-11 04:51:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,7 +14,6 @@ def map_to_square(source_array)
   source_array.map {|x| x**2  }
 end 
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point = 0)
   source_array.reduce(starting_point) {|sum, num|sum + num}
-end
 end 
