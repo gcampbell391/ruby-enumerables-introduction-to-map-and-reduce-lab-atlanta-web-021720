3be27ca0c2b4a125a17ea07cdc5718870f71c8a6
Gene Campbell III <gcampbell391@gmail.com>
2020-02-11 04:50:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,6 +18,6 @@ def reduce_to_total(source_array, starting_point)
   if starting_point != nil
   source_array.reduce(starting_point) {|sum, num|sum + num}
 else 
-  source_array.reduce() {|sum, num|sum + num}
+  source_array.reduce(0) {|sum, num|sum + num}
 end
 end 
