850f6af61d6e597ad6e3828645431f4524470cab
Gene Campbell III <gcampbell391@gmail.com>
2020-02-11 04:44:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,19 @@
-# My Code here....
+def map_to_negativize(source_array)
+  source_array.map {|x| x * -1}
+end 
+
+def map_to_no_change(source_array)
+  source_array.map {|x| x }
+end 
+
+def map_to_double(source_array)
+  source_array.map {|x| x * 2 }
+end 
+
+def map_to_square(source_array)
+  source_array.map {|x| x**2  }
+end 
+
+def reduce_to_total(source_array, starting_point)
+  source_array.reduce(starting_point) {|sum, num|sum + num}
+end 
