3687f2f649951061c07c47656aea85943c3ab739
Gene Campbell III <gcampbell391@gmail.com>
2020-02-11 04:48:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,5 +15,9 @@ def map_to_square(source_array)
 end 
 
 def reduce_to_total(source_array, starting_point)
+  if starting_point != nil
   source_array.reduce(starting_point) {|sum, num|sum + num}
+else 
+  source_array.sum 
+  
 end 
